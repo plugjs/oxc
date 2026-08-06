77e8b81617e0fd407166fda593b8590aebdd5e9e
Pier Fumagalli <pier@usrz.com>
2026-08-07 02:49:23 +0900

    Our basic configs (will be exported)

---
+++ src/configs/oxfmt.base.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from 'oxfmt'
+
+export default defineConfig({
+  ignorePatterns: [],
+
+  // Inherited Defaults
+  // "arrowParens": "always",
+  // "bracketSpacing": true,
+  // "endOfLine": "lf",
+  // "insertFinalNewline": true,
+  // "objectWrap": "preserve",
+  // "proseWrap": "preserve",
+  // "tabWidth": 2,
+  // "trailingComma": "all",
+  // "useTabs": false,
+
+  // Format JSDOC comments, defaults from OXlint are OK
+  jsdoc: {
+    lineWrappingStyle: 'balance',
+    preferCodeFences: true,
+  },
+  // Extended print width: 120 characters per line
+  printWidth: 120,
+  // Keep quotes consistent for object properties, adding when necessary
+  quoteProps: 'consistent',
+  // Never add semicolons at the ends of statements
+  semi: false,
+  // Use single quotes instead of double quotes
+  singleQuote: true,
+  // Sort imports in JS/TS files, including type imports
+  sortImports: {
+    groups: [
+      'builtin',
+      'external',
+      ['internal', 'subpath'],
+      ['parent', 'sibling', 'index'],
+      'style',
+      'type',
+      'unknown',
+    ],
+    newlinesBetween: true,
+  },
+
+  // Sort the keys in package.json files, including scripts
+  sortPackageJson: {
+    sortScripts: true,
+  },
+})
---
+++ src/configs/oxlint.node.ts
@@ -0,0 +1,42 @@
+// Re export our default configuration so that it's visible to "vscode"
+// export { default } from './src/configs/oxfmt.config.ts'
+
+import { defineConfig } from 'oxlint'
+
+import config from './oxlint.base.ts'
+
+export default defineConfig({
+  extends: [config],
+  ignorePatterns: ['test/resources'],
+  plugins: ['node'],
+  env: { ...config.env, node: true },
+
+  overrides: [
+    {
+      files: ['test/**/*'],
+      globals: {
+        describe: 'readonly',
+        fdescribe: 'readonly',
+        xdescribe: 'readonly',
+        it: 'readonly',
+        fit: 'readonly',
+        xit: 'readonly',
+        afterAll: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        beforeEach: 'readonly',
+        xafterAll: 'readonly',
+        xafterEach: 'readonly',
+        xbeforeAll: 'readonly',
+        xbeforeEach: 'readonly',
+        skip: 'readonly',
+        expect: 'readonly',
+        log: 'readonly',
+        // those are deprecated: they were used before import.meta.dirname and
+        // import.meta.filename existed on Node.js, but are still used somewhere
+        // dirnameFromUrl: 'readonly',
+        // filenameFromUrl: 'readonly',
+      },
+    },
+  ],
+})
