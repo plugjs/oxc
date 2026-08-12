0b8cea1935558ad85a44a2c565df87d45fd395e9
Pier Fumagalli <pier@usrz.com>
2026-08-13 00:15:14 +0900

    Cleanup

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,6 +1,5 @@
 // Re export our default configuration so that it's visible to "vscode"
 // export { default } from './src/configs/oxfmt.config.ts'
-
 import { defineConfig } from 'oxlint'
 
 import config from './src/configs/oxlint.node.ts'
