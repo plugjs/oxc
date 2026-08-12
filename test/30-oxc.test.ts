dd18419d5a14ed5e2bc83b8bf53b808ba3b5d7ba
Pier Fumagalli <pier@usrz.com>
2026-08-13 00:52:06 +0900

    OXC tests

--- test/30-oxc.test.ts
+++ test/30-oxc.test.ts
@@ -1,5 +1,5 @@
-import { $p, async, BuildFailure, find, mkdtemp, rmrf } from '@plugjs/plug'
-import { writeFile } from '@plugjs/plug/fs'
+import { $gry, async, BuildFailure, find, mkdtemp } from '@plugjs/plug'
+import { readFile, realpath, rm, writeFile } from '@plugjs/plug/fs'
 import { resolveAbsolutePath } from '@plugjs/plug/paths'
 import { Context } from '@plugjs/plug/pipe'
 
@@ -9,7 +9,7 @@ import type { AbsolutePath } from '@plugjs/plug'
 import type { OxfmtConfig } from 'oxfmt'
 import type { OxlintConfig } from 'oxlint'
 
-xdescribe('OXC', () => {
+describe('OXC', () => {
   let tempDir: AbsolutePath
   let context: Context
 
@@ -24,21 +24,23 @@ xdescribe('OXC', () => {
   }
 
   beforeEach(async () => {
-    tempDir = mkdtemp()
-    log.notice(`Created temporary directory ${$p(tempDir)}`)
+    tempDir = (await realpath(mkdtemp())) as AbsolutePath
     await find('resources/**/*', { directory: 'test' }).copy(tempDir)
     const buildFile = resolveAbsolutePath(tempDir, 'build.ts')
     context = new Context(buildFile, async.requireContext().taskName)
   })
 
   afterEach(async () => {
-    await rmrf(tempDir)
+    await rm(tempDir, { recursive: true })
   })
 
-  xdescribe('OXC Plug', () => {
+  describe('OXC Plug', () => {
+    beforeEach(async () => log.notice($gry('+--------------------------------------------------------------')))
+    afterEach(async () => log.notice($gry('+--------------------------------------------------------------')))
+
     it('should fail with parsing errors', () =>
       async.runAsync(context, async () => {
-        await expect(find('**/*', { directory: tempDir }).plug(new OXC())) //
+        await expect(find('**/*', { directory: tempDir }).plug(new OXC({ cwd: '@' }))) //
           .toBeRejectedWithError(BuildFailure)
       }))
 
@@ -48,45 +50,52 @@ xdescribe('OXC', () => {
           ignorePatterns: ['**/invalid*'],
         })
         await writeFormatConfig({
-          ignorePatterns: ['**/invalid*', '**/warnings.ts'],
+          ignorePatterns: ['**/invalid*'],
           semi: false,
         })
 
-        await find('**/*', { directory: tempDir }).plug(new OXC())
+        await find('**/*', { directory: tempDir }).plug(new OXC({ cwd: '@', warnOnFormat: true }))
       }))
 
-    it('should fail when no files are being linted', () =>
+    it('should fail when all files are ignored', () =>
       async.runAsync(context, async () => {
-        await writeLintConfig({ ignorePatterns: ['**/*'] })
-        await writeFormatConfig({ ignorePatterns: ['**/*'] })
+        await writeLintConfig({ ignorePatterns: ['**/*'] }, 'my-oxlint-config.json')
+        await writeFormatConfig({ ignorePatterns: ['**/*'] }, 'my-oxfmt-config.json')
 
-        await expect(find('**/*', { directory: tempDir }).plug(new OXC())) //
-          .toBeRejectedWithError(BuildFailure)
-        await expect(find('bogus.ts', { directory: tempDir }).plug(new OXC())) //
-          .toBeRejectedWithError(BuildFailure)
+        await expect(
+          find('**/test.ts', { directory: tempDir }).plug(
+            new OXC({
+              oxfmtConfig: '@my-oxfmt-config.json',
+              oxlintConfig: '@my-oxlint-config.json',
+              cwd: '@',
+            }),
+          ),
+        ).toBeRejectedWithError(BuildFailure)
+      }))
+
+    it('should fail when no files files are found', () =>
+      async.runAsync(context, async () => {
+        await expect(find('**/bozo.ts', { directory: tempDir }).plug(new OXC())).toBeRejectedWithError(BuildFailure)
       }))
   })
 
-  xdescribe('OXLint Utility', () => {
+  describe('OXLint Utility', () => {
+    beforeEach(async () => log.notice($gry('+--------------------------------------------------------------')))
+    afterEach(async () => log.notice($gry('+--------------------------------------------------------------')))
+
     it('should fail with errors', () =>
       async.runAsync(context, async () => {
-        const cwd = process.cwd()
-        try {
-          process.chdir(tempDir)
-          await expect(oxc()).toBeRejectedWithError(BuildFailure)
-        } finally {
-          process.chdir(cwd)
-        }
+        await expect(oxc({ cwd: '@' })).toBeRejectedWithError(BuildFailure)
       }))
 
     it('should succeed with warnings', () =>
       async.runAsync(context, async () => {
         await writeLintConfig({
           ignorePatterns: ['**/invalid*'],
+          rules: { 'no-console': 'warn' },
         })
         await writeFormatConfig({
-          ignorePatterns: ['**/invalid*', '**/warnings.ts'],
-          semi: false,
+          ignorePatterns: ['**/invalid*'],
         })
 
         const cwd = process.cwd()
@@ -96,6 +105,40 @@ xdescribe('OXC', () => {
         } finally {
           process.chdir(cwd)
         }
+      }))
+
+    it('should fix formatting and linting errors', () =>
+      async.runAsync(context, async () => {
+        const filename1 = resolveAbsolutePath(tempDir, 'resources/test.ts')
+        const before1 = await readFile(filename1, 'utf-8')
+        expect(before1).toEqual('console.log("This is a test");\n')
+
+        const filename2 = resolveAbsolutePath(tempDir, 'resources/warnings.ts')
+        const before2 = await readFile(filename2, 'utf-8')
+        expect(before2).toEqual('if ((!"foo") in {}) {\n}\nlet warning = true;\n')
+
+        await writeLintConfig({
+          ignorePatterns: ['**/invalid*'],
+          rules: {
+            'no-unused-vars': 'off',
+            'prefer-const': 'error',
+            'unicorn/empty-brace-spaces': 'error',
+          },
+        })
+
+        await writeFormatConfig({
+          ignorePatterns: ['**/invalid*'],
+          singleQuote: true,
+          semi: false,
+        })
+
+        await oxc({ cwd: '@', fix: true })
+
+        const after1 = await readFile(filename1, 'utf-8')
+        expect(after1).toEqual("console.log('This is a test')\n")
+
+        const after2 = await readFile(filename2, 'utf-8')
+        expect(after2).toEqual("if ((!'foo') in {}) {}\nconst warning = true\n")
       }))
   })
 })
